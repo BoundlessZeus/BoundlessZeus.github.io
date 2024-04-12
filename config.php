67cdbf350e7bc13add0fb0f86973f8d31331e097
BoundlessZeus <bigalex2125@gmail.com>
2024-04-12 02:23:09 -0500

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,10 @@
 <?php
 
-define("BASE_URL", "https://boundlesszeus.github.io/");
+define("BASE_URL", "http://boundlesszeus.github.io/");
 
 $allowedUsers = [
     "76561198097973776" => true, // buyer of the addon
+    "76561198049596575" => true, // helper
     // "76561198255091552" => true, // developer of the addon
     // "76561198251737334" => true // developer of the addon
 ];
