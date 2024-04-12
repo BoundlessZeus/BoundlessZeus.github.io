d19a08a90a91a83328332f90281fc2f633c917db
BoundlessZeus <bigalex2125@gmail.com>
2024-04-12 01:59:09 -0500

    website

---
+++ loading_screen_creator_web/config.php
@@ -0,0 +1,9 @@
+<?php
+
+define("BASE_URL", "http://127.0.0.1:8000/");
+
+$allowedUsers = [
+    "76561198097973776" => true, // buyer of the addon
+    // "76561198255091552" => true, // developer of the addon
+    // "76561198251737334" => true // developer of the addon
+];
